233ec55713ca6a6d83adf49b9dd2122aff1bf76f
Vlad Podilnyk <vladyslav.podilnyk@globallogic.com>
2017-12-13 20:22:51 +0200

    Add google tests for the project
    Add a few simple test for tcp-server/client classes.
    Also provide Cmake script in order to integrate
    tests into project build system.

---
+++ README.md
@@ -0,0 +1,30 @@
+# C++ library for networking
+
+The library provides classes for network programming.
+
+## Getting Started
+
+### Download the project
+
+```
+$ git clone https://github.com/VladPodilnyk/netlib
+```
+
+### Build project
+
+Execute instuctions below step by step.
+```
+$ mkdir build
+$ cd build
+$ cmake ..
+$ make
+```
+
+### Run tests
+
+Execute next commands in order to build and run tests.
+```
+$ make net_test && ./tests/net_test
+```
+
+
---
+++ tests/tcpserver_test.cpp
@@ -0,0 +1,81 @@
+#include "netlib/tcpserver.h"
+#include "netlib/tcpclient.h"
+#include <gtest/gtest.h>
+
+
+class ServerTest : public ::testing::Test
+{
+    public:
+        void SetUp () override {};
+        void TearDown () override {};
+};
+
+
+class ClientTest : public ::testing::Test
+{
+    public:
+        void SetUp () override {};
+        void TearDown () override {};
+};
+
+
+TEST_F (ServerTest, ServerWrongAdressThrow)
+{
+    // Arrange.
+    netpod::V1_0::config config;
+    config.addr = "abra-cadabra";
+    netpod::V1_0::Server s(config);
+
+    // Act. Assert.
+    EXPECT_THROW (s.run(), netpod::InvalidAddress);
+}
+
+
+TEST_F (ServerTest, ServerFailedToBindSocket)
+{
+    // Arrange 
+    netpod::V1_0::Config config;
+    config.addr = "ANY";
+    netpod::V1_0::Server s(config);
+
+    // Act. Assert.
+    EXPECT_THROW (s.run(), netpod::FailedToBindSocket);
+}
+
+
+TEST_F (ServerTest, CheckCreateServer)
+{
+    // Arrange, Act, Assert.
+    netpod::V1_0::Server s;
+}
+
+
+TEST_F (ClientTest, CheckCreateClient)
+{
+    // Arrange, Act, Assert.
+    netpod::V1_0::Client c;
+}
+
+
+TEST_F (ClientTest, ClientWrongAdressThrow)
+{
+    // Arrange.
+    netpod::V1_0::ClientSettings settings;
+    settings.addr = "abra-cadabra";
+    netpod::V1_0::Client c(settings);
+
+    // Act. Assert.
+    EXPECT_THROW (c.request("something"), netpod::InvalidAddress);
+}
+
+
+TEST_F (ClientTest, ClientFailedToEstablishConnection)
+{
+    // Arrange 
+    netpod::V1_0::ClientSettings settings;
+    settings.addr = "LOOPBACK";
+    netpod::V1_0::Client c(settings);
+
+    // Act. Assert.
+    EXPECT_THROW (c.request("something"), netpod::FailedToEstablishConnection);
+}
